a7378967c0c6d396fe76db69f5aa6dfd9a6f2bab
Vitor <vitor.purcino@gmail.com>
2023-02-18 19:55:01 -0400

    html e css finalizados

---
+++ src/scripts/index.js
@@ -0,0 +1,6 @@
+const btnSearch = document.querySelector('.search--button')
+const input = document.querySelector('.search--input')
+
+btnSearch.addEventListener('click', () => {
+    console.log(input.value)
+})
